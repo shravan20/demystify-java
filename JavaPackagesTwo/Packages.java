f79ddefa52009409aedfbbb2ae593fb84dfc2d8d
shravan20 <mrshravankumarb@gmail.com>
2021-03-03 23:43:20 +0530

    :sparkles: Packages Importing Concept

---
+++ JavaPackagesTwo/Packages.java
@@ -0,0 +1,33 @@
+package JavaPackagesTwo;
+
+public class Packages {
+
+    String name;
+    int age;
+    String designation;
+    double salary;
+
+    public Packages(String name){
+        this.name = name;
+    }
+
+    public void empAge(int age){
+        age = age;
+    }
+
+    public void empDesignation(String empDesig) {
+        designation = empDesig;
+     }
+
+     public void empSalary(double empSalary) {
+        salary = empSalary;
+     }
+
+     public void printEmployee() {
+        System.out.println("Name:"+ name );
+        System.out.println("Age:" + age );
+        System.out.println("Designation:" + designation );
+        System.out.println("Salary:" + salary);
+     }
+    
+}
