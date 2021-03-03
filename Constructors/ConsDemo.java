b21f30f43a59b78a96d1a927ba22b5087b4d1073
shravan20 <mrshravankumarb@gmail.com>
2021-03-03 23:55:44 +0530

    :sparkles: Constructor Message

---
+++ Constructors/ConsDemo.java
@@ -0,0 +1,9 @@
+package Constructors;
+
+public class ConsDemo {
+    public static void main(String args[]) {
+        MyClass t1 = new MyClass();
+        MyClass t2 = new MyClass();
+        System.out.println(t1.num + " " + t2.num);
+     }
+}
---
+++ Constructors/MyClass.java
@@ -0,0 +1,10 @@
+package Constructors;
+
+public class MyClass {
+   int num;
+   MyClass() {
+      num = 100;
+   }
+
+   
+}
