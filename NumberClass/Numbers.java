f1046096b9226be78f4b39782c1fb96421e233c8
shravan20 <mrshravankumarb@gmail.com>
2021-03-07 01:14:20 +0530

    :books: Added autoboxing/boxing concepts

--- NumberClass/Numbers.java
+++ NumberClass/Numbers.java
@@ -8,3 +8,8 @@ public class Numbers {
        System.out.println(x); 
     }
  }
+
+//  The object of the wrapper class contains or wraps its respective primitive data type. Converting primitive data types into object is called boxing, and this is taken care by the compiler. Therefore, while using a wrapper class you just need to pass the value of the primitive data type to the constructor of the Wrapper class.
+
+// And the Wrapper object will be converted back to a primitive data type, and this process is called unboxing. The Number class is part of the java.lang package.
+
