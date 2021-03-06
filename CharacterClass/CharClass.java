3169f7536fe0d40668f7fea2e7ce66b3deaa2900
shravan20 <mrshravankumarb@gmail.com>
2021-03-07 01:27:58 +0530

    :sparkles: Added Character autoboxing/boxing concepts

---
+++ CharacterClass/CharClass.java
@@ -0,0 +1,30 @@
+package CharacterClass;
+
+public class CharClass {
+    
+
+    public static void main(String args[]) {
+
+        char ch1 = 'a';
+
+        // Unicode for uppercase Greek omega character
+        char uniChar = '\u039A'; 
+    
+        // an array of chars
+        char[] charArray ={ 'a', 'b', 'c', 'd', 'e' }; 
+    
+        // Here following primitive char 'a'
+        // is boxed into the Character object ch
+        Character ch2 = 'a';
+    
+        // Here primitive 'x' is boxed for method test,
+        // return is unboxed to char 'c'
+        // char c = test('x');
+
+        System.out.println("She said \"Hello!\" to me.");
+
+        System.out.println("ch1:"+ch1+"\nch2:"+ch2+"\nuniChar:"+uniChar+"\ncharArray:"+charArray[0]);
+    }
+
+
+}
