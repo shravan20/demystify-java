20e818b401ee80cdc2484c4a44a49b49094546a5
shravan20 <mrshravankumarb@gmail.com>
2021-03-07 01:10:33 +0530

    :sparkles: Number Wrapper Class - Integer

---
+++ NumberClass/Numbers.java
@@ -0,0 +1,10 @@
+package NumberClass;
+
+public class Numbers {
+
+    public static void main(String args[]) {
+       Integer x = 5; // boxes int to an Integer object
+       x =  x + 10;   // unboxes the Integer to a int
+       System.out.println(x); 
+    }
+ }
