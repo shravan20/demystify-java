356122f76aaa30996b7189c103b306efdc037c3d
shravan20 <mrshravankumarb@gmail.com>
2021-03-13 15:11:52 +0530

    :sparkles: Java OOPs Extending-Interfaces : Created Hockey Interface extends Sports Interface

---
+++ ExtendingInterfaces/Hockey.java
@@ -0,0 +1,9 @@
+package ExtendingInterfaces;
+
+
+public interface Hockey extends Sports {
+    public void homeGoalScored();
+    public void visitingGoalScored();
+    public void endOfPeriod(int period);
+    public void overtimePeriod(int ot);
+ }
