bcb70190e54a50d054c4c3049e7b68b9a02b98dd
shravan20 <mrshravankumarb@gmail.com>
2021-03-13 15:52:58 +0530

    :bug: Fixed DateCompare.java - Package Path

--- JavaBasics/DateTime/DateCompare.java
+++ JavaBasics/DateTime/DateCompare.java
@@ -8,8 +8,7 @@ public class DateCompare {
 
     public static void main(String args[]) {
         Date dNow = new Date( );
-        SimpleDateFormat ft = 
-        new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+        SimpleDateFormat ft = new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
   
         System.out.println("Current Date: " + ft.format(dNow));
      }
